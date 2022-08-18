fc6563ec5fbe8096190f78fcca08d97c6b7a35f5
lagranzhai <zyk20060916@gmail.com>
2022-08-18 18:43:04 +0800

    splay done

--- splay.cpp
+++ splay.cpp
@@ -1,19 +1,13 @@
 #include<iostream>
 using namespace std;
 //最多开20e8个int
-const int MAXN=10e7;
+const int MAXN=10e5+5;
 int tot=0,rt=0;
 int fa[MAXN],ch[MAXN][2],siz[MAXN],cnt[MAXN];
 int val[MAXN];
-inline void upsiz(int x){
-    siz[x]=cnt[x]+siz[ch[x][0]]+siz[ch[x][1]];
-}
-inline bool chk(int x){
-    return x==ch[fa[x]][1];
-}
-inline void clean(int x){
-    fa[x]=ch[x][0]=ch[x][1]=siz[x]=cnt[x]=val[x]=0;
-}
+inline void upsiz(int x){siz[x]=cnt[x]+siz[ch[x][0]]+siz[ch[x][1]];}
+inline bool chk(int x){return x==ch[fa[x]][1];}
+inline void clean(int x){fa[x]=ch[x][0]=ch[x][1]=siz[x]=cnt[x]=val[x]=0;}
 void spin(int x){
     int f=fa[x],dir=chk(x),ff=fa[f];
     fa[ch[x][dir^1]]=f,ch[f][dir]=ch[x][dir^1];
@@ -24,29 +18,29 @@ void spin(int x){
 void splay(int x){
 	int f=fa[x];
 	while(f){
-		if(fa[f])spin(chk(x)==chk(f)?f:x);
+		if(fa[f])spin((chk(x)==chk(f))?f:x);
 		spin(x);
+		f=fa[x];
 	}
 	rt=x;
 }
 void insert(int v){
 	if(!rt){
-		rt=++tot,fa[tot]=0,++cnt[tot],val[tot]=v,upsiz(tot);
+		rt=++tot,fa[tot]=0,cnt[tot]++,val[tot]=v,upsiz(tot);
 		return;
 	}
 	int cur=rt,f=0;
 	while(1){
 		if(v==val[cur]){
-			cnt[cur]++;
-			upsiz(cur);
-			upsiz(f);
+			cnt[cur]++,upsiz(cur),upsiz(f);
 			splay(cur);
+			return;
 		}
 		bool dir=(v>val[cur]);
 		f=cur,cur=ch[cur][dir];
 		if(!cur){
 			fa[++tot]=f,cnt[tot]++,ch[f][dir]=tot,val[tot]=v;
-			upsiz[tot],upsiz[f];
+			upsiz(tot),upsiz(f);
 			splay(tot);
 			return;
 		}
@@ -68,11 +62,11 @@ int findx(int v){
 		}
 	}
 }
-int rank(int v){
+int rankx(int v){
 	int st=findx(v);
 	if(st==3)return 1;
 	else if(st==0||st==1)return siz[ch[rt][0]]+1;
-	else return siz[ch[rt][0]]+cnt[rt]+1;
+	else if(st==2)return siz[ch[rt][0]]+cnt[rt]+1;
 }
 int kth(int k){
 	int cur=rt;
@@ -112,7 +106,10 @@ int suf(int v){
 }
 void del(int v){
 	findx(v);
-	if(cnt[rt]>1)cnt[rt]--,upsiz(rt);
+	if(cnt[rt]>1){
+		cnt[rt]--;
+		upsiz(rt);
+	}
 	else if(!ch[rt][0]&&!ch[rt][1])clean(rt),rt=0;
 	else if(!ch[rt][0]){
 		int cur=rt;
@@ -131,7 +128,27 @@ void del(int v){
 		clean(cur),upsiz(rt);
 	}
 }
-
+int main(){
+	ios::sync_with_stdio(false);
+	//freopen("P3369_6.in","r",stdin);
+	//freopen("P3369.out","w",stdout);
+	int n=0;
+	cin>>n;
+	int opt,x;
+	for(int i=0;i<n;i++){
+		cin>>opt>>x;
+		if(opt==1)insert(x);
+		else if(opt==2)del(x);
+		else if(opt==3)cout<<rankx(x)<<"\n";
+		else if(opt==4)cout<<kth(x)<<"\n";
+		else if(opt==5)cout<<val[pre(x)]<<"\n";
+		else if(opt==6)cout<<val[suf(x)]<<"\n";
+		//cout<<i<<" ";
+	}
+	//fclose(stdin);
+	//fclose(stdout);
+	return 0;
+}
 
 
 
