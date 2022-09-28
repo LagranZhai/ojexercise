82ad801701efb0511fe615d5ee2c25375b092968
lagranzhai <zyk20060916@gmail.com>
2022-09-28 09:38:16 +0800

    1070

--- luogu/1070/1070.cpp
+++ luogu/1070/1070.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 const int maxn=1000+10;
 int n,m,p;
@@ -22,20 +22,29 @@ int main(){
 		}
 	}
 	memset(dp,0xc0,sizeof dp);
-	for(int i=1;i<=m;i++){
+    memset(mm,0xc0,sizeof mm);
+    for(int i=0;i<=n;i++){
+        dp[0][i]=0;
+    }
+    for(int i=1;i<=n;i++){
+        dp[1][i]=max(dp[1][i],val[1][i]);
+        mm[1]=max(mm[1],dp[1][i]-pri[i]);
+    }
+	for(int i=2;i<=m;i++){
 		for(int j=1;j<=n;j++){
-			dp[i][j]=mm[i-1]+val[i][j];
-			for(int k=1;k<=min(i,p);k++){
+			//dp[i][j]=mm[i-1]+val[i][j];
+			for(int k=1;k<=p&&k<i;k++){
 				if(j>k){
-					dp[i][j]=max(dp[i][j],max(dp[i-k][(j-k)],mm[i-k-1])+sum[i][j]-sum[i-k][j-k]-pri[j-k]);
+					dp[i][j]=max(dp[i][j],dp[i-k][(j-k)]+sum[i][j]-sum[i-k][j-k]-pri[j-k]);
 					//dp[i][j]-=pri[j-k];
 				}
 				else{
-					dp[i][j]=max(dp[i][j],max(dp[i-k][(j-k)+n],mm[i-k-1])+sum[i][j]+sum[i-j][n]-sum[i-k][(j-k)+n]-pri[j-k+n]);
+					dp[i][j]=max(dp[i][j],dp[i-k][(j-k)+n]+sum[i][j]+sum[i-j][n]-sum[i-k][(j-k)+n]-pri[j-k+n]);
 					//dp[i][j]-=pri[j-k+n];
 				}
 			}
-			mm[i]=max(mm[i],dp[i][j]);
+            dp[i][j]=max(dp[i][j],mm[i-1]+val[i][j]);
+			mm[i]=max(mm[i],dp[i][j]-pri[j]);
 		}
 	}                                                                                             
 	for(int i=1;i<=m;i++){
