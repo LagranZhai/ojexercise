1c26a5d0b45f244c0efcfa1673b80085874766de
boboucn <43269761+boboucn@users.noreply.github.com>
2022-10-15 22:57:04 +0800

    Create spfa.cpp
    This is a template of the Shortest Path Faster Algorithm(SPFA).

---
+++ template/graph_theory/spfa.cpp
@@ -0,0 +1,48 @@
+#include <list>
+#include <cstdio>
+
+//https://www.luogu.com.cn/problem/P3371
+
+constexpr int MAXN = int(1e4+1), MAXM = int(5e5+1);
+
+//使用链式前向星存图
+int head[MAXN], to[MAXM], next[MAXM], weight[MAXM], eid;
+inline void addedge(int u, int v, int w) { to[++eid] = v; weight[eid] = w; next[eid] = head[u]; head[u] = eid; }
+
+int dis[MAXN];
+bool isinq[MAXN];
+
+void spfa(int n, int s) {
+    for (int i = 1; i <= n; ++i)
+        dis[i] = 2147483647;
+    dis[s] = 0;
+
+    std::list<int> q;
+    q.push_back(s);
+    isinq[s] = true;
+
+    while (!q.empty()) {
+        int u = q.front(); q.pop_front(); isinq[u] = false;
+        for (int ed = head[u]; ed; ed = next[ed]) {
+            if (0LL + dis[u] + weight[ed] < dis[to[ed]]) {
+                dis[to[ed]] = dis[u] + weight[ed];
+                if (!isinq[to[ed]])
+                    q.push_back(to[ed]), isinq[to[ed]] = true;
+            }
+        }
+    }
+}
+
+int main() {
+    int n, m, s; scanf("%d%d%d", &n, &m, &s);
+    for (int i = 0; i < m; ++i) {
+        int u, v, w; scanf("%d%d%d", &u, &v, &w);
+        addedge(u, v, w);
+    }
+
+    spfa(n, s);
+
+    for (int i = 1; i <= n; ++i)
+        printf("%d ", dis[i]);
+    return 0;
+}
