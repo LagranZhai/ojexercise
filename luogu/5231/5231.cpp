9703cadc39ec18b9fa7af6dc718a60fa0353cd2e
lagranzhai <zyk20060916@gmail.com>
2022-10-18 22:53:55 +0800

    5231

--- luogu/5231/5231.cpp
+++ luogu/5231/5231.cpp
@@ -1,15 +1,28 @@
-//5357ac 
+//5231 
 #include<iostream>
+#include<cstdio>
 #include<cstring>
 #include<queue>
 #include<algorithm>
 using namespace std;
-const int maxn=2e5+5;
+const int maxn=1e5+5;
 int ch[maxn][4];
 bool cnt[maxn];
-string s,t;
+string s;
 int fail[maxn],num[maxn],jump[maxn],in[maxn],tag[maxn];
+char t[100][maxn];
+int n,m;
 int tot=0;
+inline void fr(int j,int **t){
+    char c=getchar();
+    while(c!='N'&&c!='E'&&c!='W'&&c!='S')c=getchar();
+    for(int i=0;;i++){
+        if(c=='N'){
+            t[i][j]
+        }
+    }
+        
+}
 void ins(string &s,int o){
 	int p=0;
 	for(auto c:s){
@@ -46,29 +59,29 @@ void query(string &s){
 	}
 }
 int main(){
-	ios::sync_with_stdio(false);
-	int n=0;	
-		cin>>n;
-		for(int i=0;i<n;i++){
-		 	cin>>s;
-			ins(s,i);
-			//cout<<i<<endl;
-		}
-		acam();
-		cin>>t;
-		query(t);
-		queue<int> q;
-		for(int i=1;i<=tot;i++){
-			if(!in[i])q.push(i);
-		}
-		while(!q.empty()){
-			int p=q.front();q.pop();
-			num[fail[p]]+=num[p];
-			in[fail[p]]--;
-			if(!in[fail[p]])q.push(fail[p]);
-		}
-		for(int i=0;i<n;i++){
-			cout<<num[jump[i]]<<"\n";
-		}
+	//ios::sync_with_stdio(false);	
+	cin>>n>>m;
+    
+	for(int i=0;i<n;i++){
+		cin>>s;
+		ins(s,i);
+		//cout<<i<<endl;
+	}
+	acam();
+	cin>>t;
+	query(t);
+	queue<int> q;
+	for(int i=1;i<=tot;i++){
+		if(!in[i])q.push(i);
+	}
+	while(!q.empty()){
+		int p=q.front();q.pop();
+		num[fail[p]]+=num[p];
+		in[fail[p]]--;
+		if(!in[fail[p]])q.push(fail[p]);
+	}
+	for(int i=0;i<n;i++){
+		cout<<num[jump[i]]<<"\n";
+	}
 	return 0;
 }
