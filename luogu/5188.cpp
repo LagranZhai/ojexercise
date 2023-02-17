a841ff6f2a637d5a662fe4f9cc4068b6aec9cf81
lagranzhai <zyk20060916@gmail.com>
2023-02-17 21:32:51 +0800

    1064ac

---
+++ luogu/5188.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int maxn=25;
+unordered_map<char,int> ab;
+int n,m,t;
+int a[maxn*2][maxn*2];
+void init(){
+    ab['B']=(1<<3);
+    ab['J']=(1<<2);
+    ab['M']=(1<<1);
+    ab['P']=1;
+}
+int sti(string s){
+    int _t=0;
+    for(int i=0;i<s.size();i++){
+        
+    }
+}
+int main(){
+    ios::sync_with_stdio(false);
+    init();
+    cin>>n>>m;
+    int u,v;string s;
+    for(int i=1;i<=m;i++){
+        cin>>n>>m>>s;
+
+    }
+    return 0;
+}
