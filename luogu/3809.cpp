1dd7fe69a9f50584df7ff409f891e618af493c1c
lagranzhai <zyk20060916@gmail.com>
2023-03-28 16:41:03 +0800

    new file:   luogu/3809.cpp

---
+++ luogu/3809.cpp
@@ -0,0 +1,67 @@
+#include<algorithm>
+#include<string>
+#include<iostream>
+#include<typeinfo>
+using namespace std;
+const int maxn=1e6,maxm=127;
+int rk[maxn*2+20],sa[maxn],tmp[maxn*2+20],cnt[128];
+string s;
+void solvesa(string &s){
+	int n=s.size(),m=127;
+	s=" "+s;
+	for(int i=1;i<=n;i++){ 
+		++cnt[rk[i]=s[i]];
+	}
+	for(int i=1;i<=m;i++){//int i='a'?
+		cnt[i]+=cnt[i-1];
+	}
+	for(int i=n;i>=1;i--){
+		sa[cnt[rk[i]]--]=i;
+	}
+	for(int w=1;;w*=2){
+		int num=0;
+		for(int i=n;i>n-w;i--){
+			tmp[n-i+1]=i;
+		}
+		for(int i=1,p=w;i<=n;i++){
+			if(sa[i]>w)tmp[++p]=sa[i]-w;
+		}
+		fill(cnt+1,cnt+2+m,0);
+		for(int i=1;i<=n;i++){
+			cnt[rk[tmp[i]]]++;
+		}
+		for(int i=1;i<=m;i++){
+			cnt[i]+=cnt[i-1];
+		}
+		for(int i=n;i>=1;i--){
+			sa[cnt[rk[tmp[i]]]--]=tmp[i];
+		}
+		//auto& oldrk=tmp;
+		//cout<<typeid(oldrk).name()<<endl; 
+		//copy(rk+1,rk+n+1,oldrk+1);
+		copy(rk+1,rk+n+2,tmp+1);
+		num=1;
+		rk[sa[1]]=1;
+//		auto rp=[&](int x){return make_pair(oldrk[x],oldrk[x+w]);};
+		auto rp=[&](int x){return make_pair(tmp[x],tmp[x+w]);};
+		for(int i=2;i<=n;i++){
+			rk[sa[i]]=(rp(sa[i-1])==rp(sa[i]))?num:++num;
+		}
+		if(n==num)break;
+		m=num;
+//		for(int i=1;i<=n;i++){
+//			cout<<sa[i]<<' ';
+//		}
+//		cout<<endl;
+//		getchar();
+	}
+	for(int i=1;i<=n;i++){
+		cout<<sa[i]<<' ';
+	}
+}
+int main(){
+	ios::sync_with_stdio(false);
+	cin>>s;
+	solvesa(s);
+	return 0;
+}
