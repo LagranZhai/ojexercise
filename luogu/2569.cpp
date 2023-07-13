e8e92be2ed5733cb5d8cb9a7f42def7cfc36be46
lagranzhai <zyk20060916@gmail.com>
2023-07-13 22:46:41 +0800

    modified:   luogu/2569.cpp 	new file:   luogu/libstdc++-6.dll

--- luogu/2569.cpp
+++ luogu/2569.cpp
@@ -33,24 +33,39 @@ template<> Myos<std::ostringstream>& Myos<std::ostringstream>::flush() {
 //Myos<std::ostream&> cout{std::cout};
 Myos<std::ostringstream> cout{std::ostringstream()};
 constexpr int maxn=2000;
-int T,maxp,W,ap[maxn*2],as[maxn*2],bp[maxn*2],bs[maxn*2],dp[maxn+100][maxn+100];
-
+int T,maxP,W,ap[maxn*2],as[maxn*2],bp[maxn*2],bs[maxn*2],dp[maxn+100][maxn+100];
+std::deque<int > q;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie();
-    cin>>T>>maxp>>W;
+    cin>>T>>maxP>>W;
     for(int i{1};i<=T;i++){
-        cin>>ap[i]>>as[i]>>bp[i]>>bs[i];
+        cin>>ap[i]>>bp[i]>>as[i]>>bs[i];
     }
     for(int i{1};i<=T;i++){
-        for(int j{1};j<=as[i];j++){
+        for(int j{0};j<=as[i];j++){
             dp[i][j]=-ap[i]*j;
         }
-        for(int j{1};i<=maxP;j++){
+        for(int j{0};i<=maxP;j++){
             dp[i][j]=max(dp[i][j],dp[i-1][j]);
         }
-        for()
+        if(i-W<1)continue;
+        q.clear();
+        for(int j{0};j<=maxP;j++){
+            while(!q.empty()&&j-q.front()>as[i])q.pop_front();
+            while(!q.empty()&&dp[i-W-1][q.back()]+q.back()*ap[i]<dp[i-W-1][j]+j*ap[i])q.pop_back();
+            q.push_back(j);
+            if(!q.empty())dp[i][j]=max(dp[i][j],dp[i-W-1][q.front()]+q.front()*ap[i]-j*ap[i]);
+        }
+        q.clear();
+        for(int j{maxP};j>=0;j--){
+            while(!q.empty()&&q.front()-j>bs[i])q.pop_front();
+            while(!q.empty()&&dp[i-W-1][q.back()]+q.back()*bp[i]<dp[i-W-1][j]+j*bp[i])q.pop_back();
+            q.push_back(j);
+            if(!q.empty())dp[i][j]=max(dp[i][j],dp[i-W-1][q.front()]+q.front()*bp[i]-j*bp[i]);
+        }
     }
+    cout<<dp[T][0]<<"\n";
     cout.flush();
     return 0;
 }
